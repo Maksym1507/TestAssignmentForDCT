ed1330e8dc7a3bf97b239ed697e2cb3d1a6ffd30
Max <maksym.illarionov@nure.ua>
2023-07-28 00:16:26 +0300

    empty project

---
+++ TestAssignmentForDCT/TestAssignmentForDCT/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace TestAssignmentForDCT
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
